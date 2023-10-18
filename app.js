5b9f2747ac7598beaa3db1e580a7145d8f765b2e
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-18 23:14:11 +0530

    aaddes middlewares

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    res.send( { key1: value })
+})
+const server = http.createServer(app);
 
 server.listen(3000);
