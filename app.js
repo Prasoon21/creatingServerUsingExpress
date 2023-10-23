db3ef7c28987da0b060ef16b03e657001dc700be
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-23 13:47:11 +0530

    worked on routes

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const http = require('http');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product' , (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">ADD Product</button></form>');
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+});
 
-app.use('/' , (req, res, next) => {
-    res.send('<h1>Hello from express</h1>')
-})
 const server = http.createServer(app);
 
 server.listen(3000);
