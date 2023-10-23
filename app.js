0a7175ddabe2e5a7b0d593d58f2851d26641e076
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-23 12:46:51 +0530

    added body-parser

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product' , (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">ADD Product</button></form>');
 })
 
-app.use((req, res, next) => {
-    console.log("In another middleware!");
-    res.send( { key1: value })
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/' , (req, res, next) => {
+    res.send('<h1>Hello from express</h1>')
 })
 const server = http.createServer(app);
 
