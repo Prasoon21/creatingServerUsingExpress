c254399a80bcf744c60585452dbebaae7cc9a9f8
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-25 23:14:22 +0530

    added views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,14 +7,19 @@ const app = express();
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const contactRoutes = require('./routes/contact.js');
+const successRoutes = require('./routes/success.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
+app.use(contactRoutes);
 app.use(shopRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
 });
 
 const server = http.createServer(app);
